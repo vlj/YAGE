leper/stk-code

Use deferred shading instead of light prepass

data/shaders/normalmap.frag
@@ -1,15 +1,20 @@
 #ifdef GL_ARB_bindless_texture
 layout(bindless_sampler) uniform sampler2D normalMap;
 layout(bindless_sampler) uniform sampler2D DiffuseForAlpha;
+layout(bindless_sampler) uniform sampler2D tex;
 #else
 uniform sampler2D normalMap;
 uniform sampler2D DiffuseForAlpha;
+uniform sampler2D tex;
 #endif
 
 in vec3 tangent;
 in vec3 bitangent;
 in vec2 uv;
-out vec3 EncodedNormal;
+
+layout(location = 0) out vec4 EncodedNormal_Specular_Reflectance;
+layout(location = 1) out vec4 Colors;
+layout(location = 2) out float EmitMap;
 
 vec2 EncodeNormal(vec3 n);
 
@@ -17,13 +22,24 @@ void main()
 {
     // normal in Tangent Space
     vec3 TS_normal = 2.0 * texture(normalMap, uv).rgb - 1.0;
-    float alpha = texture(DiffuseForAlpha, uv).a;
+    float gloss = texture(DiffuseForAlpha, uv).r;
+    float reflectance = texture(DiffuseForAlpha, uv).g;
     // Because of interpolation, we need to renormalize
     vec3 Frag_tangent = normalize(tangent);
     vec3 Frag_normal = normalize(cross(Frag_tangent, bitangent));
     vec3 Frag_bitangent = cross(Frag_normal, Frag_tangent);
 
     vec3 FragmentNormal = TS_normal.x * Frag_tangent + TS_normal.y * Frag_bitangent - TS_normal.z * Frag_normal;
-    EncodedNormal.xy = 0.5 * EncodeNormal(normalize(FragmentNormal)) + 0.5;
-    EncodedNormal.z = 1. - alpha;
+    EncodedNormal_Specular_Reflectance.xy = 0.5 * EncodeNormal(normalize(FragmentNormal)) + 0.5;
+    EncodedNormal_Specular_Reflectance.z = gloss;
+    EncodedNormal_Specular_Reflectance.w = reflectance;
+
+    vec4 col = texture(tex, uv);
+#ifdef GL_ARB_bindless_texture
+#ifdef SRGBBindlessFix
+    col.xyz = pow(col.xyz, vec3(2.2));
+#endif
+#endif
+    Colors = vec4(col.rgb, 1.);
+    EmitMap = texture(DiffuseForAlpha, uv).b;
 }

vlj/YAGE

data/shaders/normalmap.frag
@@ -1,15 +1,20 @@
 #ifdef Use_Bindless_Texture
 layout(bindless_sampler) uniform sampler2D normalMap;
 layout(bindless_sampler) uniform sampler2D DiffuseForAlpha;
+layout(bindless_sampler) uniform sampler2D tex;
 #else
 uniform sampler2D normalMap;
 uniform sampler2D DiffuseForAlpha;
+uniform sampler2D tex;
 #endif
 
 in vec3 tangent;
 in vec3 bitangent;
 in vec2 uv;
-out vec3 EncodedNormal;
+
+layout(location = 0) out vec4 EncodedNormal_Specular_Reflectance;
+layout(location = 1) out vec4 Colors;
+layout(location = 2) out float EmitMap;
 
 vec2 EncodeNormal(vec3 n);
 
@@ -17,13 +22,24 @@ void main()
 {
     // normal in Tangent Space
     vec3 TS_normal = 2.0 * texture(normalMap, uv).rgb - 1.0;
-    float alpha = texture(DiffuseForAlpha, uv).a;
+    float gloss = texture(DiffuseForAlpha, uv).r;
+    float reflectance = texture(DiffuseForAlpha, uv).g;
     // Because of interpolation, we need to renormalize
     vec3 Frag_tangent = normalize(tangent);
     vec3 Frag_normal = normalize(cross(Frag_tangent, bitangent));
     vec3 Frag_bitangent = cross(Frag_normal, Frag_tangent);
 
     vec3 FragmentNormal = TS_normal.x * Frag_tangent + TS_normal.y * Frag_bitangent - TS_normal.z * Frag_normal;
-    EncodedNormal.xy = 0.5 * EncodeNormal(normalize(FragmentNormal)) + 0.5;
-    EncodedNormal.z = 1. - alpha;
+    EncodedNormal_Specular_Reflectance.xy = 0.5 * EncodeNormal(normalize(FragmentNormal)) + 0.5;
+    EncodedNormal_Specular_Reflectance.z = gloss;
+    EncodedNormal_Specular_Reflectance.w = reflectance;
+
+    vec4 col = texture(tex, uv);
+#ifdef Use_Bindless_Texture
+#ifdef SRGBBindlessFix
+    col.xyz = pow(col.xyz, vec3(2.2));
+#endif
+#endif
+    Colors = vec4(col.rgb, 1.);
+    EmitMap = texture(DiffuseForAlpha, uv).b;
 }